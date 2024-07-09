af8ab3a6f63919845234976af4194dfcb8d12970
tusharbisht961 <bishttushar355@gmail.com>
2024-07-09 10:38:35 +0530

    commit

---
+++ brand/src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import HeroSection from "./components/hero"
+import Navigation from "./components/navigation";
+const App = () => {
+  return (
+    <div>
+      <Navigation />
+      <HeroSection />
+    </div>
+  );
+};
+
+export default App;
